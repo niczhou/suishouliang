733d039effbd2f10d8a015dcfc1ec7e2dadec724
niczhou <madeozhou@sina.com>
2018-02-01 17:33:59 +0800

    ontouch

--- app/src/main/java/cn/nic/suishouliang/ScaleView.java
+++ app/src/main/java/cn/nic/suishouliang/ScaleView.java
@@ -9,7 +9,10 @@ import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.Scroller;
 
 /**
  * Created by nic on 2018/1/31.
@@ -17,7 +20,11 @@ import android.view.View;
 
 public class ScaleView extends View {
     private Paint paint;
-    String direction;
+    private int direction;
+    protected Scroller scroller;
+    private int LastX;
+
+    protected OnScrollListener onScrollListener;
 
     public ScaleView(Context context) {
         super(context);
@@ -26,7 +33,9 @@ public class ScaleView extends View {
     public ScaleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray=context.obtainStyledAttributes(attrs,R.styleable.ScaleView);
-        direction=typedArray.getString(R.styleable.ScaleView_direction);
+        direction=typedArray.getInt(R.styleable.ScaleView_direction,0);
+
+        typedArray.recycle();
     }
 
     @Override
@@ -38,35 +47,61 @@ public class ScaleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawFrame(canvas);
+
+        scroller=new Scroller(getContext());
     }
 
-    private void drawFrame(Canvas canvas){
-        int w=getWidth();
-        int h=getHeight();
-        initPaint();
-        Log.i("nic.print",direction);
-//        if(direction.equals("south")) {
-//            Log.i("nic.print","direction is south");
-//            canvas.drawRect(0, 0, (float) w, (float) h, paint);
-//        }else  if(direction.equals("west")){
-//            canvas.drawOval(new RectF(0, 0, (float) w, (float) h), paint);
-//        }
-        switch (direction){
-            case "south":
-                canvas.drawRect(0, 0, (float) w, (float) h, paint);
-                break;
-            case "west":
-                canvas.drawOval(new RectF(0, 0, (float) w, (float) h), paint);
-                break;
+        private void drawFrame(Canvas canvas){
+            int w=getWidth();
+            int h=getHeight();
+            initPaint();
+            Log.i("nic.print",Integer.toString(direction));
+
+            switch (direction){
+                case 0:
+                    canvas.drawRect(0, 0, (float) w, (float) h, paint);
+                    canvas.drawLine((float)w/2,0,(float)w/2,(float)h/2,paint);
+                    break;
+                case 1:
+                    canvas.drawRect(0, 0, (float) w, (float) h, paint);
+                    canvas.drawLine((float)w,(float) h/2,(float)w/2,(float)h/2,paint);
+                    break;
+            }
         }
+        private void initPaint(){
+            paint=new Paint();
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(8);
+            paint.setColor(Color.parseColor("#fe5228"));
+        }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x= (int) event.getX();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if(scroller!=null && !scroller.isFinished()){
+                    scroller.abortAnimation();
+                    LastX=x;
+                    return true;
+                }
+            case MotionEvent.ACTION_MOVE:
+                int deltaX=LastX-x;
+                
+        }
+
+
+        return super.onTouchEvent(event);
     }
 
-    private void initPaint(){
-        paint=new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(8);
-        paint.setColor(Color.parseColor("#fe5228"));
-    }
+    private interface OnScrollListener{
+            void onScroll(int scale);
+        }
+
+        public void setOnScrollListener(OnScrollListener onScrollListener) {
+            this.onScrollListener = onScrollListener;
+        }
 
 }
