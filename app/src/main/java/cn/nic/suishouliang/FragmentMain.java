084975b578d989bba77c32efa6f827a2fbb9e295
niczhou <madeozhou@sina.com>
2018-03-06 14:45:39 +0800

    init

---
+++ app/src/main/java/cn/nic/suishouliang/FragmentMain.java
@@ -0,0 +1,21 @@
+package cn.nic.suishouliang;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nic on 2018/3/6.
+ */
+
+public class FragmentMain extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
