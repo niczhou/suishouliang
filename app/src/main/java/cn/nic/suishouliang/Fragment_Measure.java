b146ef598344564a40e23141fcf84af55741cc36
niczhou <madeozhou@sina.com>
2018-03-26 17:08:17 +0800

    todo sharedpreference

--- app/src/main/java/cn/nic/suishouliang/Fragment_Measure.java
+++ app/src/main/java/cn/nic/suishouliang/Fragment_Measure.java
@@ -9,6 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nic on 2018/3/6.
@@ -21,6 +25,16 @@ public class Fragment_Measure extends Fragment implements View.OnClickListener {
     private Bundle bundle;
     private Button btn_redraw;
     private Button btn_reset;
+    private EditText et_phone;
+    private EditText et_weight;
+    private EditText et_height;
+    private EditText et_width;
+    private EditText et_mnorth;
+    private EditText et_msouth;
+    private EditText et_mwest;
+    private EditText et_meast;
+    private List<EditText> list_et;
+    private boolean isEditable = false;
 
 
     @Nullable
@@ -34,6 +48,24 @@ public class Fragment_Measure extends Fragment implements View.OnClickListener {
         btn_redraw.setOnClickListener(this);
         btn_reset = (Button) view.findViewById(R.id.btn_reset);
         btn_reset.setOnClickListener(this);
+
+        et_phone = (EditText) view.findViewById(R.id.et_phone);
+        et_weight = (EditText) view.findViewById(R.id.et_weight);
+        et_height = (EditText) view.findViewById(R.id.et_height);
+        et_width = (EditText) view.findViewById(R.id.et_width);
+        et_mnorth = (EditText) view.findViewById(R.id.et_mnorth);
+        et_msouth = (EditText) view.findViewById(R.id.et_msouth);
+        et_mwest = (EditText) view.findViewById(R.id.et_mwest);
+        et_meast = (EditText) view.findViewById(R.id.et_meast);
+        list_et = new ArrayList<>();
+        list_et.add(et_phone);
+        list_et.add(et_weight);
+        list_et.add(et_height);
+        list_et.add(et_width);
+        list_et.add(et_mnorth);
+        list_et.add(et_msouth);
+        list_et.add(et_mwest);
+        list_et.add(et_meast);
 
         return view;
     }
@@ -56,9 +88,25 @@ public class Fragment_Measure extends Fragment implements View.OnClickListener {
                 break;
             case R.id.btn_reset:
                 bundle.putString("measure","reset");
+                reset();
                 break;
         }
         intent.putExtras(bundle);
         localBroadcastManager.sendBroadcast(intent);
     }
+        private void reset(){
+            if(isEditable){
+                for (EditText et:list_et) {
+                    et.setEnabled(false);
+                }
+                btn_reset.setText("自定义");
+                isEditable = false;
+            }else{
+                for (EditText et:list_et) {
+                    et.setEnabled(true);
+                }
+                btn_reset.setText("确定");
+                isEditable = true;
+            }
+        }
 }
---
+++ app/src/main/java/cn/nic/suishouliang/NxUtil.java
@@ -0,0 +1,22 @@
+package cn.nic.suishouliang;
+
+import android.content.SharedPreferences;
+import android.os.StrictMode;
+import android.widget.EditText;
+
+/**
+ * Created by nic on 2018/3/26.
+ */
+
+public class NxUtil {
+
+    protected void push_data( String key, String value,SharedPreferences sharedPreferences){
+
+    }
+    protected void sys_data(){
+
+    }
+    protected void db_connect(){
+
+    }
+}
